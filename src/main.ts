a9f4f761579d1419a35df5758e5c1cf0a05c1825
Subodh <subodhsigdel63@gmail.com>
2026-09-25 00:18:44 +0545

    chore : added validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app/app.module.js';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //this is used to validate the incoming request data and transform it into the desired format. It also removes any properties that are not defined in the DTO.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //whitelist: true, //this will remove any properties that are not defined in the DTO
+      whitelist: true,
+      //forbidNonWhitelisted: true, //this will throw an error if any properties that are not defined in the DTO are present in the request
+      forbidNonWhitelisted: true,
+      //transform: true, //this will transform the incoming request data into the desired format
+      transform: true,
+    }),
+  );
+  //this is used to handle all the exceptions thrown in the application and return a proper response to the client
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Task Management API')
