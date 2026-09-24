0a68d2e382d22e31cd2fb75d4c87a9491ca7c188
Subodh <subodhsigdel63@gmail.com>
2026-09-24 23:15:58 +0545

    feat : create user and task entity for migration test

---
+++ src/modules/tasks/entities/task.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { User } from '../../users/entities/user.entity.js';
+
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string | null;
+
+  @Column({ name: 'is_completed', default: false })
+  isCompleted: boolean;
+
+  @Column({ name: 'user_id' })
+  userId: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.tasks, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+}
